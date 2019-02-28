67344eb8564b85cef49ca56b4f6085a2ebb12ff4
Deb Outlaw <outlaw2925@aol.com>
2019-02-28 15:53:55 -0500

    update

--- JS/jQuery.js
+++ JS/jQuery.js
@@ -1,5 +1,26 @@
 //mark off todos
-$("li").click(function() {
-    $(this).css("color", "gray");
-    $(this).css("text-decoration", "line-through");
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+//delete todos
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    //stop bubbling  w/jQuery
+    event.stopPropagation();
+
+});
+
+//add todos
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        //grab text
+        let todoText = $(this).val();
+        //clear text box
+        $(this).val("");
+        //create new <li></li>
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
